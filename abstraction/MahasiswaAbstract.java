96f1742acbfe7aadb4073a3359958bedc31748d7
tita <inititaa23@gmail.com>
2025-10-03 14:52:52 +0700

    Menambahkan file MahasiswaAbstract.java difolder abstraction

---
+++ abstraction/MahasiswaAbstract.java
@@ -0,0 +1,27 @@
+abstract class Mahasiswa {
+    protected String nama;
+
+    public Mahasiswa(String nama) {
+        this.nama = nama;
+    }
+
+    public abstract void belajar();
+}
+
+class MahasiswaS1 extends Mahasiswa {
+    public MahasiswaS1(String nama) {
+        super(nama);
+    }
+
+    @Override
+    public void belajar() {
+        System.out.println(nama + " belajar mata kuliah S1.");
+    }
+}
+
+public class MahasiswaAbstract {
+    public static void main(String[] args) {
+        Mahasiswa mhs = new MahasiswaS1("Tita");
+        mhs.belajar();
+    }
+}
